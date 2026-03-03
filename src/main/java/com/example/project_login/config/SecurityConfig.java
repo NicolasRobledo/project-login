a2aeeba0b78244e06724a8464d037554106c5ea3
CallapaBraian <braiancallapa@gmail.com>
2026-03-03 16:36:34 -0300

    roble gay

--- src/main/java/com/example/project_login/config/SecurityConfig.java
+++ src/main/java/com/example/project_login/config/SecurityConfig.java
@@ -2,23 +2,24 @@ package com.example.project_login.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(authz -> authz
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/hola").permitAll()
                 .anyRequest().authenticated()
             )
-            .httpBasic(basic -> {});
-        
+            .httpBasic(Customizer.withDefaults())
+            .formLogin(form -> form.disable());
+
         return http.build();
     }
 }
