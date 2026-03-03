d3180e5d6461a9410c5483fd0ea1388586615323
Nicolas Robledo <tomatekiwi84@gmail.com>
2026-03-03 16:56:22 -0300

    cayapagey

--- src/main/java/com/example/project_login/config/SecurityConfig.java
+++ src/main/java/com/example/project_login/config/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +26,23 @@ public class SecurityConfig {
             .formLogin(form -> form.disable());
 
         return http.build();
+    }
+
+    // Usuario en memoria
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("1234")) // obligatorio encriptado
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    // Password encoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
